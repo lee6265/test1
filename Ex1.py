008ad28cde8082f9f3677a224f50fc4f99aa742c
tllee <tllee@cute.edu.tw>
2021-09-15 10:56:55 +0800

    modify Ex1 perimeter

--- Ex1.py
+++ Ex1.py
@@ -1,4 +1,6 @@
 import math
 r = eval(input('r='))
 area = math.pi * r ** 2
+perimeter = 2 * math.pi * r
 print('circle area = {:.2f}'.format(area))
+print('circle perimeter = {:.2f}'.format(perimeter))
