a9ec2ad8eb8efd7c208bb88b60d26709b92dea5c
tllee <tllee@cute.edu.tw>
2025-02-13 12:33:13 +0800

    week 2

---
+++ ch2/ex2-1.py
@@ -0,0 +1,11 @@
+import datetime
+day = datetime.date(2025,2,13)
+#today = datetime.date.today() 
+print(f"week2: {day}")
+print('-' * 60)
+fruitsList= ['Banana', 'Apple', 'Grape', 'Pineapple', 'Orange']
+print(fruitsList)
+print('水果種類: {}'.format(len(fruitsList)))
+print('第三種水果是: {}'.format(fruitsList[2]))
+print('最後一種水果是: {}'.format(fruitsList[-1]))
+print('前三種水果是: {}'.format(fruitsList[:3]))
